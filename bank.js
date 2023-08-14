f3712858b67aa5d1bdc5198e83935260e8ec1a63
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-15 02:57:10 +0600

    add

--- bank.js
+++ bank.js
@@ -1,21 +1,47 @@
 // Bank.js javascript work
 
 // diposit + withdraw + Amount=======All amount 
-const dipositAmount=document.getElementById('dipositAmount');
-const withdrawAmount=document.getElementById('withdrawAmount');
-const totalAmount=document.getElementById('totalAmount');
+const dipositAmount = document.getElementById('dipositAmount');
+const withdrawAmount = document.getElementById('withdrawAmount');
+const totalAmount = document.getElementById('totalAmount');
 
 
 // diposit and Withdraw amount
-const dipositInput=document.getElementById('dipositInput');
-const dipositButton=document.getElementById('dipositButton');
+const dipositInput = document.getElementById('dipositInput');
+const dipositButton = document.getElementById('dipositButton');
 
-const withdrawInput=document.getElementById('withdrawInput');
-const withdrawButton=document.getElementById('withdrawButton');
+const withdrawInput = document.getElementById('withdrawInput');
+const withdrawButton = document.getElementById('withdrawButton');
 
-dipositButton.addEventListener('click',function () {  
-    const dipoIn=dipositInput.innerText;
-    dipositAmount.innerText=dipoIn;
-    console.log(dipositAmount);
+// dipositButton.addEventListener('click',function () {
+//     // console.log('ok');
+//     const dipoinput=dipositInput.value;
+//     console.log(dipoinput);
+// })
+
+dipositButton.addEventListener('click', function () {
+    const newdipoInString = dipositInput.value;
+    const newDipoNum=parseFloat(newdipoInString);
+    const preAmount = dipositAmount.innerText;
+    const newAmount=parseFloat(preAmount);
+    const newTotalAmount=newDipoNum+newAmount;
+
+    dipositAmount.innerText = newTotalAmount;
+
+
+    
+    // console.log(typeof newDipoNum);
+
+
+    // console.log(dipositAmount);
+    // if (dipositAmount == 'number') {
+    //     dipositAmount.innerText = newdipoIn;
+    // } else {
+    //     // alert('sorry')
+    // }
+
+
+    // 
+    dipositInput.value = '';
 })
 // console.log("ok");
