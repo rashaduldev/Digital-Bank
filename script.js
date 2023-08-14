753f007c590f67796237ead92c65400967b58169
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-14 22:17:32 +0600

    add

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ const user_btn=document.getElementById('user_btn');
 user_btn.addEventListener('click',function () {
     // console.log(user_btn);
     const em=user_email.value;
-    console.log(em);
+    // console.log(em);
+    if (em==='r@g.com' && user_password==='abcd') {
+        console.log('vallied input');
+    } else {
+        console.log("invallied input");
+    }
 })
