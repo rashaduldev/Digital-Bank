a1e42aeb9dd2e46b9902580b79ac4c2a61fe03aa
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-14 21:59:36 +0600

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+const user_email=document.getElementById('user_email');
+const user_password=document.getElementById('user_password');
+const user_btn=document.getElementById('user_btn');
+
+
+user_btn.addEventListener('click',function () {
+    // console.log(user_btn);
+    const em=user_email.value;
+    console.log(em);
+})
