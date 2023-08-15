cfe1f863a05057606d3b391aa96545872347a237
Rashadul Islam <rashadul.dev@gmail.com>
2023-08-16 00:10:12 +0600

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -1,4 +1,5 @@
 const allWithdrawAmount=document.getElementById('withdrawAmount');
+const allTotalAmount = document.getElementById('totalAmount');
 
 const withdrawInput = document.getElementById('withdrawInput');
 const withdrawButton = document.getElementById('withdrawButton');
@@ -9,6 +10,30 @@ withdrawButton.addEventListener('click', function () {
     if (isNaN(newInputValue) || newInputValue<0) {
         alert('Please providee a positive number');
     } else {
-        alert('good');
+       const withdrawAmount=allWithdrawAmount.innerText;
+    //  allWithdrawAmount.innerText=newInputValue;
+
+    //  withdraw amonunt parse float
+    const oldWithdrawAmount=allWithdrawAmount.innerText;
+    const newWithdrawAmount=parseFloat(oldWithdrawAmount);
+    // console.log(newWithdrawAmount);
+    // console.log(newInputValue);
+     const newpOld= newWithdrawAmount+newInputValue;
+     allWithdrawAmount.innerText=newpOld;
+
+     const preTotalAm=allTotalAmount.innerText;
+     const newTotalAm=parseFloat(preTotalAm);
+     // console.log(typeof newTotalAm);
+
+     const currallTotalAmount=newTotalAm-newInputValue;
+    //  const zeroLesAmount=currallTotalAmount;
+     if (currallTotalAmount<0) {
+        alert('Please select Balance Amount');
+     } else {
+        allTotalAmount.innerText=currallTotalAmount;
+     }
+     
+
+   withdrawInput.value='';
     }
 });
